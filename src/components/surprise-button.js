dcd3a5035f50a33ea20f9cc9a205b502eaa0430d
Joe Turner <joe@oampo.co.uk>
2017-02-14 13:59:35 +0000

    Starting point.

--- src/components/surprise-button.js
+++ src/components/surprise-button.js
@@ -1,5 +1,2 @@
 import React from 'react';
 
-export default function SurpriseImage(props) {
-    return <button onClick={props.onClick}>Surprise!</button>;
-}
--- src/components/surprise-image.js
+++ src/components/surprise-image.js
@@ -1,5 +1,2 @@
 import React from 'react';
 
-export default function SurpriseImage() {
-    return <img src="http://i.giphy.com/ueVYG9Cg4zBu0.gif" alt="A surprising horse" />;
-}
