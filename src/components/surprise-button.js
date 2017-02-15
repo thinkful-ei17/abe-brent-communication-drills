48ba63f9c707ee4ccc511b2448869f34d0253532
Joe Turner <joe@oampo.co.uk>
2017-02-15 14:16:22 +0000

    Add initial components in starting point.

--- src/components/surprise-button.js
+++ src/components/surprise-button.js
@@ -1,2 +1,6 @@
 import React from 'react';
 
+export default function SurpriseButton(props) {
+    return <button>Surprise!</button>;
+}
+
--- src/components/surprise.js
+++ src/components/surprise.js
@@ -1,2 +1,15 @@
 import React from 'react';
 
+import SurpriseButton from './surprise-button';
+import SurpriseImage from './surprise-image';
+
+export default class Surprise extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        // Show the button to start with
+        return <SurpriseButton />;
+    }
+}
