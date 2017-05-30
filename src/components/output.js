480b06fb8f1a01c35021d063038ee819b686a331
Joe Turner <joe@oampo.co.uk>
2017-05-30 13:43:46 +0100

    Add aria-live

--- src/components/output.js
+++ src/components/output.js
@@ -4,7 +4,7 @@ export default function Output(props) {
     return (
         <div className="form-group">
             <label htmlFor={props.id}>{props.label}</label>
-            <output id={props.id}>
+            <output id={props.id} aria-live="polite">
                 ${props.value}
             </output>
         </div>
