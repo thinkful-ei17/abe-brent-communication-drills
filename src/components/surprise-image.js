549498cd516cfdd840c45f5ba026ea0056c5ec67
Joe Turner <joe@oampo.co.uk>
2017-06-21 13:50:48 +0100

    Fix alt tag.

--- src/components/surprise-image.js
+++ src/components/surprise-image.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function SurpriseImage() {
-    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="A surprising horse" />;
+    return <img src="https://media.giphy.com/media/10dV5LFkPenFxm/giphy.gif" alt="A surprising fig" />;
 }
