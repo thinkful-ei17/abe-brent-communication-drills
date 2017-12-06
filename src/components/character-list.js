6f79f84a5b3053f174d36ec23caa77c55fc97213
Elias Mason <eliasjmason@gmail.com>
2017-12-06 04:43:12 -0800

    Minor cleanup

--- src/components/character-list.js
+++ src/components/character-list.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function CharacterList(props) {
     const characters = props.characters.map((character, index) =>
         <li key={index}>
-            <strong>{character.name}</strong> ({character.actor}) - {character.description}</li>
+            <strong>{character.name}</strong> ({character.actor}) - {character.description}
+        </li>
     );
     return (
         <ul className="character-list">
--- src/components/live-search.js
+++ src/components/live-search.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import SearchForm from './search-form';
-import CharacterList from './character-list';
 import CharacterCount from './character-count';
+import CharacterList from './character-list';
 
 export default function LiveSearch(props) {
     return (
