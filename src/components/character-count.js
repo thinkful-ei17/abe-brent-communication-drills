4ba6b09fec5b896633c3018f743df8b9e7f5cf15
Joe Turner <joe@oampo.co.uk>
2017-12-11 11:38:07 +0000

    Move character count CSS to its own file.

--- src/components/character-count.js
+++ src/components/character-count.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import './character-count.css';
+
 export default function CharacterCount(props) {
     return (
-    <span
-        aria-live="assertive"
-        aria-atomic="true"
-        id="character-count"
-        className="character-count"
-        role="status"
-    >
-        {props.count} characters
-    </span>
-  );
+        <span
+            aria-live="assertive"
+            aria-atomic="true"
+            id="character-count"
+            className="character-count"
+            role="status">
+            {props.count} characters
+        </span>
+    );
 }
